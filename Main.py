3fc74910d2f29d10e622ebbb138e9e1b7fb8d25f
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-02-07 16:27:44 +0500

    added the wheather logic

---
+++ Main.py
@@ -0,0 +1,150 @@
+import sys
+import requests
+from PyQt5.QtWidgets import QApplication , QWidget , QLabel , QLineEdit , QPushButton , QVBoxLayout
+from PyQt5.QtCore import Qt
+
+class WheatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enther city name: " , self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_time_button = QPushButton("Get Weather and Time: " , self)
+        self.time_label = QLabel("08:17 AM" , self)
+        self.temperature_label = QLabel( self)
+        self.emoji_label = QLabel(self)
+        self.description_label = QLabel(self)
+        self.iniUI()
+
+
+    def iniUI(self):
+
+
+    
+        self.setWindowTitle("Time and Weather")
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_time_button)
+        vbox.addWidget(self.time_label)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_time_button.setObjectName("get_weather_time_button")
+        self.time_label.setObjectName("time_label")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        self.setStyleSheet("""
+
+            QLabel , QpushButton{
+                        font-family : calibri;      
+                            }
+            QLabel#city_label{
+                        font-size : 40px;
+                        font-style : italic;   
+                           }
+            QLineEdit#city_input{
+                        font-size : 40px;   
+                           }
+            QPushButton#get_weather_time_button{
+                        font-size : 30px;   
+                        font-weight : bold;
+                           }
+            QLabel#temperature_label{
+                        font-size : 75px;    
+                           }              
+            QLabel#time_label{
+                        font-size : 75px;    
+                           } 
+            QLabel#emoji_label{
+                        font-size : 100px;  
+                        font-family : Segoe UI emoji;    
+                           }
+            QLabel#description_label{
+                        font-size : 50px;   
+                           }
+        """
+
+        )
+
+        self.get_weather_time_button.clicked.connect(self.get_weather)
+        self.get_weather_time_button.clicked.connect(self.get_time)
+    def get_weather(self):
+        
+        api_key = "edd82bf56cd40475e9c211bff3bdb700"
+        city = self.city_input.text()
+
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            if data["cod"] == 200:
+                self.display_weather(data)
+        except requests.exceptions.HTTPError as http_error:
+            match response.status_code:
+                case 400:
+                    self.display_error("Bad request:\nPlease check your input")
+                case 401:
+                    self.display_error("Unauthorized:\nInvalid Api Key")
+                case 403:
+                    self.display_error("Forbidden:\nAccess is denied")
+                case 404:
+                    self.display_error("Not Found:\nCity not found")
+                case 500:
+                    self.display_error("Internal Server Error:\nPlease try again later")
+                case 502:
+                    self.display_error("Bad Gateway:\nInvalid response from the server")
+                case 503:
+                    self.display_error("Service Unavailable:\nServer is down")
+                case 504:
+                    self.display_error("Gateway Timeout:\nNo response from the server")
+                case _:
+                    self.display_error(f"Http error occured\n{http_error}")
+        except requests.exceptions.ConnectionError:
+            self.display_error("Connection Error:\nCheck your Internet connection")
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout Error:\nThe request timed out")
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Too many Redirects:\nCheck the URL")
+        except requests.exceptions.RequestException as req_error:
+            self.display_error(f"Redirect Error:\n{req_error}")
+
+
+    def get_time(self):
+        city = self.city_input.text().lower()
+
+    def display_error(self , message):
+        self.temperature_label.setStyleSheet("font-size: 30px;")
+        self.temperature_label.setText(message)
+
+    def display_weather(self , data):
+        self.temperature_label.setStyleSheet("font-size: 75px;")
+        temprature_k = data["main"]["temp"]
+        temprature_c = temprature_k - 273.15
+
+        self.temperature_label.setText(f"{temprature_c:.0f}°C")
+
+    def display_time (self , data):
+        pass   
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    wheather_app = WheatherApp()
+    wheather_app.show()
+    sys.exit(app.exec_())
